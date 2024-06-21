9f4bfea364f8105f74794f329be15b3bde578cb6
shahpnmlab <34034134+shahpnmlab@users.noreply.github.com>
2024-06-21 16:12:38 +0100

    QoL updates

--- warper/main.py
+++ warper/main.py
@@ -10,13 +10,25 @@ logger = logging.getLogger()
 
 warper = typer.Typer(add_completion=False)
 
+app_version = 0.3
+def version_callback(value: bool):
+    if value:
+        typer.echo(f"Version: {app_version}")
+        raise typer.Exit()
 
 @warper.command(no_args_is_help=True)
-def run(config_file: Path = typer.Option(..., "--config", "-c", help="input configuration file (eg. config.ini)"),
-        out_path: Path = typer.Option(None, "--output", "-o", help="Output name for bash script"),
-        submission_script_template: Path = typer.Option(None, "-s", "--submission_script",
-                                                        help="Path to a submission script template")):
+def main(config_file: Path = typer.Option(None, "--config", "-c", help="input configuration file (eg. config.ini)"),
+         out_path: Path = typer.Option(None, "--output", "-o", help="Output name for bash script"),
+         submission_script_template: Path = typer.Option(None, "--submission_script", "-s",
+                                                         help="Path to a submission script template"),
+         version: bool = typer.Option(None, "--version", callback=version_callback, is_eager=True, help="Show the version and exit")):
+
+    if not config_file:
+        typer.echo("Error: Missing option '--config' / '-c'.")
+        raise typer.Exit(code=1)
+
     settings = parser(config_file)
+
     if out_path is None:
         filename = os.path.join(os.getcwd(), f"warpTools_{datetime.datetime.now():%Y%m%d_%H%M%S}.sh")
     else:
@@ -35,18 +47,14 @@ def run(config_file: Path = typer.Option(..., "--config", "-c", help="input conf
             file.write(f'{com}\n')
 
     # Log all commands after they have been written to the file
-    logger.info(
-        f"Generating a command script {filename}\nScript contains the following commands:\n" + "\n".join(commands))
+    logger.info(f"Generating a command script {filename}\nScript contains the following commands:\n" + "\n".join(commands))
 
-    if submission_script_template is None:
-        pass
-    else:
-        if submission_script_template.is_file():
-            with open(submission_script_template, "a") as sub:
-                sub.write("\n")
-                sub.write(f"bash {filename}")
-                sub.write("\n")
-
+    if submission_script_template is not None and submission_script_template.is_file():
+        with open(submission_script_template, "a") as sub:
+            sub.write("\n")
+            sub.write(f"bash {filename}")
+            sub.write("\n")
 
 if __name__ == "__main__":
-    warper()(run)
+    warper()(main)
+
